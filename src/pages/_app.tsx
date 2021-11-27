534be891327259df08702975b3f47390795bf97f
mfbz <michael.fabozzi@icloud.com>
2021-11-27 17:01:19 +0100

    Little store persistence update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import Head from 'next/head';
 import { getNeoLineWallet, getO3Wallet, getWalletConnectWallet } from '@rentfuse-labs/neo-wallet-adapter-wallets';
 import { Application } from '@application';
@@ -15,30 +15,36 @@ require('@rentfuse-labs/neo-wallet-adapter-ant-design/styles.css');
 
 export default function _App({ Component, pageProps }: AppProps) {
 	// The pages of the application to handle routing and title displaying
-	const pages = [{ url: '/', title: 'Invocations', icon: <AppstoreOutlined /> }];
+	const pages = useMemo(() => {
+		return [{ url: '/', title: 'Invocations', icon: <AppstoreOutlined /> }];
+	}, []);
 
 	// The Neo N3 wallets i can connect to
-	const wallets = [
-		getNeoLineWallet(),
-		getO3Wallet(),
-		getWalletConnectWallet({
-			options: {
-				chainId: 'neo3:testnet',
-				methods: ['invokefunction'],
-				appMetadata: {
-					name: 'MyApplicationName', // your application name to be displayed on the wallet
-					description: 'My Application description', // description to be shown on the wallet
-					url: 'https://myapplicationdescription.app/', // url to be linked on the wallet
-					icons: ['https://myapplicationdescription.app/myappicon.png'], // icon to be shown on the wallet
+	const wallets = useMemo(() => {
+		return [
+			getNeoLineWallet(),
+			getO3Wallet(),
+			getWalletConnectWallet({
+				options: {
+					chainId: 'neo3:testnet',
+					methods: ['invokefunction'],
+					appMetadata: {
+						name: 'MyApplicationName', // your application name to be displayed on the wallet
+						description: 'My Application description', // description to be shown on the wallet
+						url: 'https://myapplicationdescription.app/', // url to be linked on the wallet
+						icons: ['https://myapplicationdescription.app/myappicon.png'], // icon to be shown on the wallet
+					},
 				},
-			},
-			logger: 'debug',
-			relayProvider: 'wss://relay.walletconnect.org',
-		}),
-	];
+				logger: 'debug',
+				relayProvider: 'wss://relay.walletconnect.org',
+			}),
+		];
+	}, []);
 
-	// Create the store
-	const store = createRootStore();
+	// Create the store in this way to apply persistence
+	const store = useMemo(() => {
+		return createRootStore();
+	}, []);
 	// Apply persistence to the store
 	useEffect(() => {
 		if (typeof window !== 'undefined') {
@@ -52,7 +58,7 @@ export default function _App({ Component, pageProps }: AppProps) {
 				},
 			});
 		}
-	}, []);
+	}, [store]);
 
 	// Note that we don't recommend ever replacing the value of a Provider with a different one
 	// Using MobX, there should be no need for that, since the observable that is shared can be updated itself
