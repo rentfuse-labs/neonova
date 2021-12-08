6eb5a9d04d32678afc336867d345a9d75c8f9bbc
mfbz <michael.fabozzi@icloud.com>
2021-12-08 18:23:52 +0100

    Updated base app component with support of dynamic wallet depending on network type

--- i18n.js
+++ i18n.js
@@ -3,7 +3,7 @@ const { localeSubpaths } = require("next/config").default().publicRuntimeConfig;
 const path = require("path");
 
 module.exports = new NextI18Next({
-  otherLanguages: ["en", "tr"],
+  otherLanguages: ["en", "it"],
   defaultLanguage: "en",
   localeSubpaths,
   localePath: path.resolve("./public/locales"),
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,72 @@ import { FormOutlined, CodeOutlined } from '@ant-design/icons';
 import { Application } from '@application';
 import { WalletModalProvider } from '@rentfuse-labs/neo-wallet-adapter-ant-design';
 import { WalletProvider } from '@rentfuse-labs/neo-wallet-adapter-react';
-import { getNeoLineWallet, getO3Wallet, getWalletConnectWallet } from '@rentfuse-labs/neo-wallet-adapter-wallets';
-import { createRootStore, persist, RootStoreProvider } from '@stores';
+import {
+	getNeoLineWallet,
+	getO3Wallet,
+	getWalletConnectWallet,
+	Wallet,
+} from '@rentfuse-labs/neo-wallet-adapter-wallets';
+import { createRootStore, persist, RootStoreProvider, SettingsNetworkType, useRootStore } from '@stores';
+import { autorun } from 'mobx';
+import { observer } from 'mobx-react-lite';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { LocalWalletProvider } from 'src/wallet';
 
 // Use require instead of import, and order matters
 require('@styles/global.css');
 require('@rentfuse-labs/neo-wallet-adapter-ant-design/styles.css');
+
+// Return available wallets depending on network type
+function getWallets(networkType: SettingsNetworkType) {
+	if (networkType === 'TestNet' || networkType === 'MainNet') {
+		return [
+			getNeoLineWallet(),
+			getO3Wallet(),
+			getWalletConnectWallet({
+				options: {
+					chainId: networkType === 'TestNet' ? 'neo3:testnet' : 'neo3:mainnet',
+					methods: ['invokefunction'],
+					appMetadata: {
+						name: 'Neonova',
+						description: 'Like Postman but for NEO N3.',
+						url: 'https://neonova.space',
+						icons: ['https://raw.githubusercontent.com/rentfuse-labs/neonova/main/neonova-icon.png'],
+					},
+				},
+				logger: 'debug',
+				relayProvider: 'wss://relay.walletconnect.org',
+			}),
+		];
+	}
+	return [];
+}
+
+// Changes available wallets depending on network type
+const GlobalWalletProvider = observer(function GlobalWalletProvider({ children }: { children: React.ReactNode }) {
+	const { settingsStore } = useRootStore();
+
+	// The Neo N3 wallets i can connect to
+	const [wallets, setWallets] = useState<Wallet[]>([]);
+	// When network types changes update wallets
+	useEffect(() => {
+		setWallets(getWallets(settingsStore.network.type));
+	}, [settingsStore.network.type]);
+
+	return (
+		<>
+			<WalletProvider wallets={wallets} autoConnect={false}>
+				<WalletModalProvider centered={false}>
+					<LocalWalletProvider>{children}</LocalWalletProvider>
+				</WalletModalProvider>
+			</WalletProvider>
+
+			<style jsx>{``}</style>
+		</>
+	);
+});
 
 export default function _App({ Component, pageProps }: AppProps) {
 	// The pages of the application to handle routing and title displaying
@@ -19,30 +75,6 @@ export default function _App({ Component, pageProps }: AppProps) {
 		return [
 			{ url: '/', title: 'Invocations', icon: <CodeOutlined /> },
 			{ url: '/contract', title: 'Contracts', icon: <FormOutlined /> },
-		];
-	}, []);
-
-	// The Neo N3 wallets i can connect to
-	const wallets = useMemo(() => {
-		return [
-			getNeoLineWallet(),
-			getO3Wallet(),
-			getWalletConnectWallet({
-				options: {
-					chainId: 'neo3:testnet',
-					methods: ['invokefunction'],
-					appMetadata: {
-						name: 'Neonova',
-						description: 'Like Postman but for NEO N3.',
-						url: 'https://github.com/rentfuse-labs',
-						icons: [
-							'https://raw.githubusercontent.com/rentfuse-labs/neo-wallet-adapter/main/neo-wallet-adapter_icon.png',
-						],
-					},
-				},
-				logger: 'debug',
-				relayProvider: 'wss://connect.coz.io',
-			}),
 		];
 	}, []);
 
@@ -60,7 +92,7 @@ export default function _App({ Component, pageProps }: AppProps) {
 					if (data) {
 						// To remove loading indicator if it was present when saved snapshot
 						const dataJson = JSON.parse(data);
-						return {...dataJson, viewStore: {...dataJson.viewStore, loadingVisible: false}};
+						return { ...dataJson, viewStore: { ...dataJson.viewStore, loadingVisible: false } };
 					}
 					return null;
 				},
@@ -75,21 +107,17 @@ export default function _App({ Component, pageProps }: AppProps) {
 	// Using MobX, there should be no need for that, since the observable that is shared can be updated itself
 	return (
 		<RootStoreProvider value={store}>
-			<WalletProvider wallets={wallets} autoConnect={false}>
-				<WalletModalProvider centered={false}>
-					<LocalWalletProvider>
-						<Application pages={pages}>
-							<Head>
-								<link rel="shortcut icon" href="favicon/favicon.ico" />
-								<title>Neonova</title>
-								<meta name="description" content="Like Postman but for NEO N3." />
-							</Head>
+			<GlobalWalletProvider>
+				<Application pages={pages}>
+					<Head>
+						<link rel="shortcut icon" href="favicon/favicon.ico" />
+						<title>Neonova</title>
+						<meta name="description" content="Like Postman but for NEO N3." />
+					</Head>
 
-							<Component {...pageProps} />
-						</Application>
-					</LocalWalletProvider>
-				</WalletModalProvider>
-			</WalletProvider>
+					<Component {...pageProps} />
+				</Application>
+			</GlobalWalletProvider>
 		</RootStoreProvider>
 	);
 }
